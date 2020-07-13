3fd6366babe3579d8ae5392ee2740189518abbee
David Nuescheler <david@nuescheler.ch>
2020-07-13 09:18:12 -0700

    feat: initial homepage

--- scripts/twp3.js
+++ scripts/twp3.js
@@ -1,7 +1,48 @@
-function decoratePage() {
-    console.log('decorate twp3')
+async function fetchSteps() {
+    var resp=await fetch('steps.json');
+    return (await resp.json());
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
+async function insertSteps() {
+    const $steps=document.querySelector('main div.steps');
+    if ($steps) {
+        const steps=await fetchSteps();
+        let html='';
+        steps.forEach((step, i) => {
+            html+=`<div class="card">
+                <div class='img' style="background-image: url(${step.Thumbnail})"></div>
+                <div class='text'>
+                    <div><h4>${step.Title}</h4>
+                    <p>${step.Title}</p>
+                    </div>
+                    <a href="step.html?${i}">${step.CTA}</a>
+                </div>
+            </div>`
+        })
+        $steps.innerHTML=html;
+    }
+}
+
+function decoratePage() {
+
+    //find steps marker
+    document.querySelectorAll('main p').forEach(($e) => {
+        if ($e.innerHTML.toLowerCase().trim()=='&lt;steps&gt;') {
+            $e.parentNode.classList.add('steps');
+            $e.parentNode.innerHTML='';
+        }
+    })
+
+    insertSteps();
+}
+
+console.log(document.readyState)
+if (document.readyState == 'loading') {
+    window.addEventListener('DOMContentLoaded', (event) => {
+        decoratePage();
+    });
+} else {
     decoratePage();
-});
+}
+
+
