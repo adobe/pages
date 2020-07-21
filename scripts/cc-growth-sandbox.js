8a17b1ce5023c17fbd8f4e10a3370ea13fff3edf
ispykenny <kenny.krosky@gmail.com>
2020-07-21 10:34:05 -0700

    added styles for hero

--- scripts/cc-growth-sandbox.js
+++ scripts/cc-growth-sandbox.js
@@ -1,1 +1,25 @@
-console.log('here')
+console.log('loaded')
+
+
+function createHeroElement() {
+  let hero = document.querySelectorAll('.default')[0];
+  let heroBackground = hero.querySelector('img').getAttribute('src');
+  let heroTitle = hero.querySelector('h1').innerText;
+  let heroCopy = hero.querySelectorAll('p')[1].innerText;
+  
+  hero.innerHTML = `
+    <div class="hero">
+      <div class="inner hero__inner">
+        <div class="hero_content">
+          <h1>${heroTitle}</h1>
+          <p>${heroCopy}</p>
+        </div>
+      </div>
+
+      <div class="hero-background" style="background-image: url(${heroBackground});">
+      </div>
+    </div>
+  ` 
+}
+
+createHeroElement();
