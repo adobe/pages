a1e1e6fe6cf377e27ffab424929d3bbb43a23009
ispykenny <kenny.krosky@gmail.com>
2020-07-21 16:25:45 -0700

    refactor hero element

--- scripts/cc-growth-sandbox.js
+++ scripts/cc-growth-sandbox.js
@@ -1,25 +1,18 @@
-console.log('loaded')
-
-
 function createHeroElement() {
-  let hero = document.querySelectorAll('.default')[0];
+  let hero = document.querySelectorAll('.title')[0];
   let heroBackground = hero.querySelector('img').getAttribute('src');
-  let heroTitle = hero.querySelector('h1').innerText;
-  let heroCopy = hero.querySelectorAll('p')[1].innerText;
+  let heroContent = hero.querySelector('.header');
   
   hero.innerHTML = `
     <div class="hero">
       <div class="inner hero__inner">
-        <div class="hero_content">
-          <h1>${heroTitle}</h1>
-          <p>${heroCopy}</p>
-        </div>
+        ${heroContent.innerHTML}
       </div>
-
       <div class="hero-background" style="background-image: url(${heroBackground});">
       </div>
     </div>
   ` 
 }
 
-createHeroElement();
+
+window.addEventListener('DOMContentLoaded', createHeroElement());
