031d1a51d9aa22bf6ab721cda901bebd18e897ed
ispykenny <kenny.krosky@gmail.com>
2020-07-22 10:04:36 -0700

    added header/footer structure

--- scripts/cc-growth-sandbox.js
+++ scripts/cc-growth-sandbox.js
@@ -1,26 +1,26 @@
-function createHeroElement() {
-  let hero = document.querySelectorAll('.title')[0];
-  let heroBackground = hero.querySelector('img').getAttribute('src');
-  let heroContent = hero.querySelector('.header');
-  
-  hero.innerHTML = `
-    <div class="hero">
-      <div class="inner hero__inner">
-        ${heroContent.innerHTML}
+function createHeroElement($element) {
+  let hero = $element;
+  if(!$element) return;
+    let heroBackground = hero.querySelector('img').getAttribute('src');
+    let heroContent = hero.querySelector('.header');
+    
+    hero.innerHTML = `
+      <div class="hero">
+        <div class="inner hero__inner">
+          ${heroContent.innerHTML}
+        </div>
+        <div class="hero-background" style="background-image: url(${heroBackground});">
+        </div>
       </div>
-      <div class="hero-background" style="background-image: url(${heroBackground});">
-      </div>
-    </div>
-  ` 
+    ` 
 }
-
 
 
 
 if (document.readyState == 'loading') {
   window.addEventListener('DOMContentLoaded', (event) => {
-    createHeroElement()
+    createHeroElement(document.querySelectorAll('.title')[0])
   });
 } else {
-  createHeroElement()
+  createHeroElement(document.querySelectorAll('.title')[0]);
 }
