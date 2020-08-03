69536a119470850eff2cd7d972c419c2d3a7ec4c
David Nuescheler <david@nuescheler.ch>
2020-08-03 12:31:58 -0700

    feat: decorateTables()

--- scripts/default.js
+++ scripts/default.js
@@ -152,7 +152,7 @@ function paramHelper() {
 async function decoratePage() {
     unwrapEmbeds();
     turnListSectionIntoCards();
-    turnTableSectionIntoCards();
+    decorateTables();
     smartWrap('main>div.default');
     decorateForm();
     await loadLocalFooter();
@@ -198,30 +198,47 @@ function formatListCard($li) {
   }
   
 
-  function turnTableSectionIntoCards() {
+  function toClassName(name) {
+    return (name.toLowerCase().replace(/[^0-9a-z]/gi, '-'))
+  }
+
+  function decorateTables() {
     document.querySelectorAll('main div.default>table').forEach(($table) => {
         const $cols=$table.querySelectorAll('thead tr th');
-        const cols=Array.from($cols).map((e) => e.innerHTML);
+        const cols=Array.from($cols).map((e) => toClassName(e.innerHTML));
         const $rows=$table.querySelectorAll('tbody tr');
-        const $cards=createTag('div', {class:'cards'})
-        $rows.forEach(($tr) => {
-            const $card=createTag('div', {class:'card'})
-            $tr.querySelectorAll('td').forEach(($td, i) => {
-                const $div=createTag('div', {class: cols[i]});
-                const $a=$td.querySelector('a[href]');
-                if ($a && $a.getAttribute('href').startsWith('https://www.youtube.com/')) {
-                    const yturl=new URL($a.getAttribute('href'));
-                    const vid=yturl.searchParams.get('v');
-                    $div.innerHTML=`<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;"><iframe src="https://www.youtube.com/embed/${vid}?rel=0" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen scrolling="no" allow="encrypted-media; accelerometer; gyroscope; picture-in-picture"></iframe></div>`;
-                } else {
-                    $div.innerHTML=$td.innerHTML;
-                }
-                $card.append($div);
-            });
-            $cards.append($card);
+        let $div={};
+
+        if (cols.length==1 && $rows.length==1) {
+            $div=createTag('div', {class:`${cols[0]}`});
+            $div.innerHTML=$rows[0].querySelector('td').innerHTML;
+        } else {
+            $div=turnTableSectionIntoCards($table, cols) 
+        }
+        $table.parentNode.replaceChild($div, $table);
+    });
+  }
+
+  function turnTableSectionIntoCards($table, cols) {
+    const $rows=$table.querySelectorAll('tbody tr');
+    const $cards=createTag('div', {class:`cards ${cols.join('-')}`})
+    $rows.forEach(($tr) => {
+        const $card=createTag('div', {class:'card'})
+        $tr.querySelectorAll('td').forEach(($td, i) => {
+            const $div=createTag('div', {class: cols[i]});
+            const $a=$td.querySelector('a[href]');
+            if ($a && $a.getAttribute('href').startsWith('https://www.youtube.com/')) {
+                const yturl=new URL($a.getAttribute('href'));
+                const vid=yturl.searchParams.get('v');
+                $div.innerHTML=`<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;"><iframe src="https://www.youtube.com/embed/${vid}?rel=0" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen scrolling="no" allow="encrypted-media; accelerometer; gyroscope; picture-in-picture"></iframe></div>`;
+            } else {
+                $div.innerHTML=$td.innerHTML;
+            }
+            $card.append($div);
         });
-        $table.parentNode.replaceChild($cards, $table);
-    })
+        $cards.append($card);
+    });
+    return ($cards);
   }
 
 
