74f9babe41512c855cf18cddd4f41ffd4e21388c
ispykenny <kenny.krosky@gmail.com>
2020-08-04 15:21:53 -0700

    added styles for main video

--- scripts/default.js
+++ scripts/default.js
@@ -178,10 +178,10 @@ function paramHelper() {
     // make sure video indicator is being requested
     if(query_type.get('v')) {
         let video_index = query_type.get('v') - 1;
-        let parent_wrapper = document.querySelector('.cards').parentElement;
+        let parent_wrapper = document.querySelector('.cards');
         let mainVideo = document.createElement('div');
         mainVideo.setAttribute('class', 'main-video');
-        mainVideo.innerHTML = document.querySelectorAll('.cards li')[video_index].innerHTML;
+        mainVideo.innerHTML = document.querySelectorAll('.cards .card')[video_index].innerHTML;
         parent_wrapper.prepend(mainVideo);
         // document.querySelectorAll('.cards li')[video_index].style.display = 'none'
     } 
