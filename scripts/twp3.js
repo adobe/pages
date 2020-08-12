3563a10f3e3f02b48ae42063019a555e35cfaab3
ispykenny <kenny.krosky@gmail.com>
2020-08-12 10:53:03 -0700

    added styles for illustrator TL

--- scripts/twp3.js
+++ scripts/twp3.js
@@ -42,20 +42,29 @@ async function insertSteps() {
 }
 
 function addNavCarrot() {
-  if(document.querySelector('header img')) {
-    let carrot = document.createElement('span');
-    carrot.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+  if(document.querySelector('header svg')) {
+    let svg = document.querySelector('header svg');
+    console.log(svg)
+    let svgWithCarrot = document.createElement('div');
+    svgWithCarrot.classList.add('nav-logo');
+
+    svgWithCarrot.innerHTML = `
+      <span class="product-icon">
+        ${svg.outerHTML}
+      </span>
+
+      <span class="carrot">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+      </span>
     `;
-    document.querySelector('header img')
-    .closest('p')
-    .append(carrot);
+    svg.remove();
+    document.querySelector('header div')
+    .prepend(svgWithCarrot);
   }
 }
 
 
 function dropDownMenu() {
-    console.log('here')
   let $header = document.querySelector('header');
 
   if(window.outerWidth >= 768) return;
@@ -215,8 +224,8 @@ async function decoratePage() {
     // nav style/dropdown
     addNavCarrot();
 
-    if(document.querySelector('header img')) {
-      document.querySelector('header p').addEventListener('click', dropDownMenu)
+    if(document.querySelector('.nav-logo')) {
+      document.querySelector('.nav-logo').addEventListener('click', dropDownMenu)
     }
 
     let pageType;
---
+++ scripts/twpds.js
@@ -0,0 +1,126 @@
+export function playVideo() {
+  document.getElementById('placeholder').classList.add('hidden');
+  const $video=document.getElementById('video');
+  $video.classList.remove('hidden');
+  $video.play();
+}
+
+function decorateVideoSections() {
+
+      // videos
+      document.querySelectorAll('main a[href^="https://images-tv.adobe.com/"]').forEach(($vlink) => {
+          const $videoDiv=$vlink.closest('div');
+          const href=$vlink.getAttribute('href');
+          const imgSrc=$videoDiv.querySelector('img').getAttribute('src');
+          $videoDiv.classList.add('video-section');
+  
+          const $videoText=createTag('div', {class: 'video-text'});
+  
+          Array.from($videoDiv.children).forEach(($e) => {
+              if (!$e.querySelector('img')) {
+                  $videoText.append($e);
+              }
+          });
+  
+          $videoDiv.innerHTML=`<div class="video"><div id="placeholder" class="button">
+          <svg xmlns="http://www.w3.org/2000/svg"><use href="/static/twp3/icons/play.svg#icon"></use></svg>
+          </div>
+          <video id='video' class="hidden" preload="metadata" src="${href}" tabindex="0">
+          <source src="${href}" type="video/mpeg4">
+          </video></div>`;
+          $videoDiv.append($videoText);
+  
+          $videoDiv.firstChild.firstChild.style.backgroundImage=`url(${imgSrc})`;
+          $videoDiv.firstChild.addEventListener('click', (e) => playVideo());
+          
+      })    
+}
+
+async function insertSteps() {
+  const $steps=document.querySelector('main div.steps');
+  if ($steps) {
+      let i=1;
+      let done=false;
+      let html='';
+      do {
+          const resp=await fetch (`step-${i}.plain.html`);
+          if (resp.status == 200) {
+              const text=await resp.text();
+              const $html=createTag('div');
+              $html.innerHTML=text; 
+              const $h1=$html.querySelector('h1');
+              const title=$h1?$h1.innerHTML:'';
+              let desc='';
+              if (title) {
+                  const $p=$h1.nextElementSibling;
+                  if ($p) desc=$p.innerHTML;
+              }
+              const $h4=$html.querySelector('h4');
+              const duration=$h4?$h4.innerHTML.split('|')[1]:'';
+              const $ul=$html.querySelector('div:first-of-type ul');
+              const greatfor=$ul?$ul.innerHTML:'';
+              const $img=$html.querySelector('img');
+              const src=$img?$img.getAttribute('src'):'';
+
+              html+=`<div class="card" onclick="window.location='step-${i}'">
+              <div class="card-header">
+                  <span class="card-duration">${duration} | Great for</span>
+                  <div class="card-greatfor"><ul>${greatfor}</ul></div>
+              </div>
+              <div class="card-img" style="background-image: url(${src})">
+              <svg xmlns="http://www.w3.org/2000/svg"><use href="/static/twp3/icons/play.svg#icon"></use></svg>
+              </div>
+              <div class="card-text">
+                  <div><h4>${title}</h4>
+                  <p>${desc}</p>
+                  </div>
+                  <a href="step-${i}">Watch now</a>
+              </div>
+              </div>`
+
+          } else {
+              done=true;
+          }
+          i++;
+
+      } while (!done);
+
+      $steps.innerHTML=html;
+      fixIcons();
+  }
+}
+
+async function decoratePage() {
+
+  await loadLocalHeader();
+
+  // heading
+  classify ('main>div:nth-of-type(1)', 'header');
+  classify ('main>.header ul', 'roles');
+
+  classify ('main h3', 'gray', 1)
+
+  decorateVideoSections();
+
+  document.querySelectorAll('main p').forEach(($e) => {
+      if ($e.innerHTML.toLowerCase().trim()=='&lt;steps&gt;') {
+          $e.parentNode.classList.add('steps');
+          $e.parentNode.classList.remove('default');
+          $e.parentNode.innerHTML='';
+      }
+  })
+
+  await insertSteps();
+
+  window.pages.decorated = true;
+  appearMain();
+
+}
+
+if (document.readyState == 'loading') {
+  window.addEventListener('DOMContentLoaded', (event) => {
+      decoratePage();
+  });
+} else {
+  decoratePage();
+}
