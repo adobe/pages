4103cd1557fa48a10dfea2d1f37ffcbf2822f93c
Kenny Krosky <kenny.krosky@gmail.com>
2021-01-25 14:57:33 -0800

    added timeline for mission blocks

--- templates/default/iframe.js
+++ templates/default/iframe.js
@@ -7,4 +7,39 @@ function generateVideo() {
   })
 }
 
+
+function setUpTimeline() {
+  if(!document.querySelector('.missiontimeline-container')) return;
+  const timelineParent = document.querySelector('.missiontimeline');
+  const timelineItems = document.querySelectorAll('.missiontimeline > div');
+  let timelineCount = 0;
+  const timelineWrapper = document.createElement('div');
+  timelineWrapper.classList.add('timeline-items')
+  let timelineGroup = '';
+
+  timelineItems.forEach(function(item, index) {
+
+    if(index < timelineItems.length - 1) {
+    
+      item.firstChild.parentElement.className=`checklist-item ${item.firstChild.innerText.toLowerCase()}`;
+      item.firstChild.remove();
+      timelineGroup += item.outerHTML;
+      timelineCount = index + 1
+      item.remove();
+
+    }
+
+
+
+  })
+
+  console.log(timelineGroup)
+  timelineWrapper.innerHTML = timelineGroup;
+  timelineParent.prepend(timelineWrapper)
+
+
+}
+
+
+setUpTimeline();
 generateVideo();
