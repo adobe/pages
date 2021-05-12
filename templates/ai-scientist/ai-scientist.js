654c428f868b37da7e41d06bc8c3942021b7e7f1
Kenny Krosky <kenny.krosky@gmail.com>
2021-05-12 14:42:13 -0700

    icons pack and nav styles
    icons for illustration scientist  and nav style edits for tl template

--- templates/ai-scientist/ai-scientist.js
+++ templates/ai-scientist/ai-scientist.js
@@ -39,7 +39,7 @@ function addNavCarrot() {
 
   
 
-      if(document.querySelector('header .section-wrapper').children[1].firstElementChild.nodeName === "P") {
+      if(document.querySelector('header .section-wrapper p')) {
           let productName = document.querySelector('header .section-wrapper').children[1].querySelector('p')
           document.querySelector('.product-icon').appendChild(productName)            
       }
@@ -97,22 +97,83 @@ function wrapSections(element) {
       const $wrapper=createTag('div', { class: 'section-wrapper'});
       $div.parentNode.appendChild($wrapper);
       $wrapper.appendChild($div);
+      $wrapper.firstElementChild.classList.add('container')
   });
 }
 
 
 function decorateHero() {
+  let svg = document.createElement('div');
+  svg.className = 'hero-banner';
+  const svg_path = `${window.location.origin}/templates/ai-scientist/assets/hero.svg`
 
+  svg.innerHTML = `<img src="${svg_path}">`
+  // svg.innerHTML = `./assets/hero.svg`
+  
+  document.querySelector('.hero .container').prepend(svg)
+
+  
 
 }
-
 
 async function decorateHome() {
   document.body.classList.add('home');
 
   document.querySelector('main .default:first-of-type').classList.add('hero')
-
   decorateHero();
+
+  const $section_two = document.querySelector('main .default:nth-child(2)');
+
+  let li = '';
+
+  // icon set up on homepage
+  $section_two.querySelectorAll('li').forEach((list) => {
+    let icon_title = list.innerText.split('-')[0]
+    let icon_type = list.innerText.split('-')[1].trim()
+    li += `
+      <li class="${icon_type}">
+        <span><img src="${window.location.origin}/templates/ai-scientist/assets/${icon_type}.svg"></span>
+        <span>${icon_title}</span>
+      </li>
+    `
+  })
+
+  $section_two.querySelector('ul').innerHTML = li;
+  $section_two.querySelector('ul').className = 'topic_icons';
+
+  // set up video section on homepage
+  const $section_three = document.querySelector('main .default:nth-child(3) .container');
+  const $video_url = $section_three.querySelector('a')
+  $video_url.parentElement.remove();
+  const $video_content = $section_three.innerHTML;
+  $section_three.innerHTML = ``
+
+  $section_three.innerHTML = `
+    <div class="video__wrapper">
+      <div class="video__element">
+        <video src="${$video_url.getAttribute('href')}">
+          <source src="${$video_url.getAttribute('href')}">
+          </video>
+          <button class="video__play-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="78.092" height="78.092" viewBox="0 0 78.092 78.092">
+              <path id="Path_1" data-name="Path 1" d="M41.1,2.05A39.046,39.046,0,1,0,80.142,41.1,39.046,39.046,0,0,0,41.1,2.05ZM61.761,42.787,32.239,60.12a3.924,3.924,0,0,1-2.08.6h-2.8A1.962,1.962,0,0,1,25.4,58.755V23.437a1.962,1.962,0,0,1,1.962-1.962h2.8a3.924,3.924,0,0,1,2.08.6L61.761,39.4a1.962,1.962,0,0,1,0,3.383Z" transform="translate(-2.05 -2.05)" fill="#4e86ff"/>
+            </svg>
+        
+          </button>
+
+      </div>
+      <div class="video__content">
+        ${$video_content}
+      </div>
+    </div>
+  
+  `
+  
+  document.querySelector('.video__play-button').addEventListener('click', (event) => {
+    document.querySelector('.video__element video').play();
+    document.querySelector('.video__element video').setAttribute('controls', true);
+    event.currentTarget.remove();
+  })
 
   document.querySelectorAll('main p').forEach(($e) => {
       const inner=$e.innerHTML.toLowerCase().trim();
@@ -127,8 +188,8 @@ async function decorateHome() {
 
 
 async function decoratePage() {
+  wrapSections('main>div');
   addDefaultClass('main>div');
-
   await loadLocalHeader();
 
   externalLinks('header');
