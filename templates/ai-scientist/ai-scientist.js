e7082fa0392b37a3ec00336147e64c0d2274ac0c
Kenny Krosky <kenny.krosky@gmail.com>
2021-05-14 21:28:52 -0700

    dynamic nav for ai-scientist template

--- templates/ai-scientist/ai-scientist.js
+++ templates/ai-scientist/ai-scientist.js
@@ -5,16 +5,15 @@ async function fetchSteps() {
   return (Array.isArray(json) ? json : json.data);
 }
 
-function getThumbnail(step) {
-  let thumbnail=step.Thumbnail;
-  if (!thumbnail) {
-      if (step.Video.startsWith('https://www.youtube.com')) {
-          const yturl=new URL(step.Video);
-          const vid=yturl.searchParams.get('v');    
-          thumbnail=`https://img.youtube.com/vi/${vid}/0.jpg`;
-      }
-  }
-  return (thumbnail);
+
+
+function decorateFooter() {
+  const create_footer_wave = document.createElement('div');
+  create_footer_wave.className = 'footer-wave';
+  create_footer_wave.innerHTML = `
+    <img src="${window.location.origin}/templates/ai-scientist/assets/footer-wave.svg"/>
+  `
+  document.querySelector('main .default:last-of-type').append(create_footer_wave);
 }
 
 function addNavCarrot() {
@@ -50,15 +49,30 @@ function addNavCarrot() {
 const icon_cleanup = ($string) => {
   let original = $string.split('\n')
   let type = '';
-  original.forEach((icon_set) => {
-    if(icon_set.split('-')[1] != undefined) {
-      type += `
-        <div class="great_for_icon_set">
-          <img src="${window.location.origin}/templates/ai-scientist/assets/${icon_set.split('-')[1].trim().toLowerCase()}.svg">
-        </div>
-      `
-    }
-  })
+  if(document.getElementsByTagName('body')[0].classList.contains('home')) {
+    original.forEach((icon_set) => {
+      if(icon_set.split('-')[1] != undefined) {
+        type += `
+          <div class="great_for_icon_set">
+            <img src="${window.location.origin}/templates/ai-scientist/assets/${icon_set.split('-')[1].trim().toLowerCase()}.svg">
+          </div>
+        `
+      }
+    })
+  } else {
+    original.forEach((icon_set) => {
+      if(icon_set.split('-')[1] != undefined) {
+        type += `
+          <li class="great_for_icon_set">
+            <span>
+              <img src="${window.location.origin}/templates/ai-scientist/assets/${icon_set.split('-')[1].trim().toLowerCase()}.svg">
+            </span>
+            <span>${icon_set.split('-')[0].trim()}</span>
+          </li>
+        `
+      }
+    })
+  }
   return type;
 }
 
@@ -83,7 +97,7 @@ async function insertSteps() {
                 ${icons}
               </div>
             </div>
-            <a href="/step?${index + 1}" class="steps__image">
+            <a href="step?${index + 1}" class="steps__image">
               <img src="${step.Thumbnail}"/>
             </a>
 
@@ -100,15 +114,154 @@ async function insertSteps() {
   }
 }
 
+function style_timeline($string) {
+  const cleanup = $string.split('\n');
+  let li = '';
+  cleanup.forEach((list_item) => {
+    li += `<li>${list_item}</li>`
+  })
+  return li;
+}
+
+
+function video_style ($video_url) {
+  const video = `
+  <div class="video__wrapper">
+    <div class="video__element">
+      <video src="${$video_url.Video}">
+        <source src="${$video_url.Video}">
+      </video>
+      <button class="video__play-button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="78.092" height="78.092" viewBox="0 0 78.092 78.092">
+          <path id="Path_1" data-name="Path 1" d="M41.1,2.05A39.046,39.046,0,1,0,80.142,41.1,39.046,39.046,0,0,0,41.1,2.05ZM61.761,42.787,32.239,60.12a3.924,3.924,0,0,1-2.08.6h-2.8A1.962,1.962,0,0,1,25.4,58.755V23.437a1.962,1.962,0,0,1,1.962-1.962h2.8a3.924,3.924,0,0,1,2.08.6L61.761,39.4a1.962,1.962,0,0,1,0,3.383Z" transform="translate(-2.05 -2.05)" fill="#4e86ff"/>
+        </svg>
+      </button>
+    </div>
+    <div class="video-info-single">
+      <span><strong>${$video_url.Title}</strong></span>
+      <span class="spacer">|</span>
+      <span>${$video_url.Duration}</span>
+    </div>
+    <ul class="video-timeline">
+      ${style_timeline($video_url.Video_timeline)}
+    </ul>
+  </div>
+  `
+  return video;
+}
+
 
 
 async function decorateStep() {
   document.body.classList.add('step');
+  document.querySelector('main .default:first-of-type').classList.add('hero')
+  decorateHero();
+
+  const $intro = document.querySelector('main .default:nth-child(1)');
+  const $icon_set = document.querySelector('main .default:nth-child(2)');
+  const $step_two = document.querySelector('main .default:nth-child(3)');
+  const $step_three = document.querySelector('main .default:nth-child(4)');
+
+  $icon_set.className = 'icon-set'
+  $step_two.className = 'step-two'
+  $step_three.className = 'step-three'
+
   const stepIndex=(+window.location.search.substring(1).split('&')[0])-1;
   const steps=await fetchSteps();
   let next_video = '';
   let next_video_index = stepIndex + 1;
   const currentStep=steps[stepIndex];
+
+  console.table(currentStep)
+
+  // hero
+  $intro.querySelector('h1').innerText = currentStep.Title
+  $intro.querySelector('p').innerText = currentStep.Description
+
+  // icon section
+  $icon_set.querySelector('p').remove();
+  const create_icon_row = document.createElement('ul');
+  create_icon_row.className = 'topic_icons';
+  create_icon_row.innerHTML = icon_cleanup(currentStep.Great_for);
+  $icon_set.querySelector('.container').append(create_icon_row)
+
+  // step two - video section
+  const video_element = document.createElement('div');
+  video_element.className = 'step-video';
+  video_element.innerHTML = video_style(currentStep) 
+  $step_three.querySelector('.container').append(video_element)
+
+  const nav = $step_three.querySelector('ul');
+  nav.className = 'mini-nav'
+  $step_three.querySelector('ul').remove()
+
+  console.log(stepIndex)
+  if(stepIndex === 0) {
+    
+  }
+
+  document.querySelector('.video__wrapper').append(nav)
+
+
+  if(stepIndex === 0) {
+    document.querySelector('.mini-nav li:last-of-type').remove() 
+  } else {
+    document.querySelector('.mini-nav li:last-of-type a').setAttribute('href', `step?${stepIndex}`)
+    document.querySelector('.mini-nav li:last-of-type a').innerText = steps[stepIndex-1].Title
+  }
+
+  if(stepIndex + 1 < steps.length) {
+    console.log('not last video')
+    document.querySelector('.mini-nav li:nth-child(1) a').setAttribute('href', `step?${stepIndex+2}`)
+    document.querySelector('.mini-nav li:nth-child(1) a').innerText = steps[stepIndex+1].Title
+  } else {
+    console.log('last video')
+    document.querySelector('.mini-nav li:nth-child(1)').remove(); 
+  }
+  
+
+  // get oriented
+  // set up video section on homepage
+  const $video_card = document.querySelector('main .default:nth-child(5) .container');
+  document.querySelector('main .default:nth-child(5)').classList.add('video_card')
+  const $video_url = $video_card.querySelector('a')
+  $video_url.parentElement.remove();
+  const $video_content = $video_card.innerHTML;
+  $video_card.innerHTML = ``
+
+  $video_card.innerHTML = `
+    <div class="video__wrapper">
+      <div class="video__element">
+        <video src="${$video_url.getAttribute('href')}">
+          <source src="${$video_url.getAttribute('href')}">
+          </video>
+          <button class="video__play-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="78.092" height="78.092" viewBox="0 0 78.092 78.092">
+              <path id="Path_1" data-name="Path 1" d="M41.1,2.05A39.046,39.046,0,1,0,80.142,41.1,39.046,39.046,0,0,0,41.1,2.05ZM61.761,42.787,32.239,60.12a3.924,3.924,0,0,1-2.08.6h-2.8A1.962,1.962,0,0,1,25.4,58.755V23.437a1.962,1.962,0,0,1,1.962-1.962h2.8a3.924,3.924,0,0,1,2.08.6L61.761,39.4a1.962,1.962,0,0,1,0,3.383Z" transform="translate(-2.05 -2.05)" fill="#4e86ff"/>
+            </svg>
+        
+          </button>
+
+      </div>
+      <div class="video__content">
+        ${$video_content}
+      </div>
+    </div>
+  
+  `
+  
+  document.querySelector('.video__play-button').addEventListener('click', (event) => {
+    document.querySelector('.video__element video').play();
+    document.querySelector('.video__element video').setAttribute('controls', true);
+    event.currentTarget.remove();
+  })
+
+
+
+
+  // style footer 
+  decorateFooter();
+
 }
 
 function wrapSections(element) {
@@ -202,6 +355,8 @@ async function decorateHome() {
       }
   })
   await insertSteps();
+
+  decorateFooter();
 
 }
 
