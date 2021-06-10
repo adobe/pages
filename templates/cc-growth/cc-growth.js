a04f9965f0da94f9978aafa5aaabaa6f721a6b40
Kenny Krosky <kenny.krosky@gmail.com>
2021-06-10 11:28:26 -0700

    icon library lazy-load refactor

--- templates/cc-growth/cc-growth.js
+++ templates/cc-growth/cc-growth.js
@@ -1,110 +1,293 @@
-loadJSModule(`/scripts/default.js`);
+// simple form submission
+async function submitSheetForm($form, sheetid, thankyou) {
+    const formsink='https://script.google.com/macros/s/AKfycbxWFwI-qExw0Tg_LJvdisSYODFw35m3L8M5HdumPOufmArmRIEh/exec'
+    let searchParams = new URLSearchParams(`?sheet-id=${sheetid}`);
+    if ($form.reportValidity()) {
+        $form.querySelectorAll(".form-field").forEach(($f) => {
+            if ($f.getAttribute('type') == 'radio') {
+                if ($f.checked) searchParams.append($f.name, $f.value);
+            } else {
+                searchParams.append($f.name, $f.value);
+            }
+        })
+        const resp=await fetch(formsink+'?'+searchParams.toString());
+        const json=await resp.json();
+        if (json.status == 'ok') {
+            window.location=thankyou;
+        } else {
+            alert ('Form Submission failed.')
+            console.log(`form submission error: ${json.description}`);
+        }    
+    }
+}
 
-const items = [];
-let has_searched = false;
 
-const set_icon_layout = ($color) => {
-  const icons = document.querySelectorAll('main li');
+function createButtonCta(element) {
+    if(!document.querySelectorAll(element).length > 0) return;
+    let button = document.querySelectorAll(element);
+    button.forEach(function($link) {
+        if($link.innerText.includes('[cta')) {
+            let cta_strings = $link.innerText.split('[')[1].split(']')[0]
+            console.log(cta_strings)
+            $link.setAttribute('class', 'button '+cta_strings)
+            $link.innerText = $link.innerText.split('[')[0]
+        }
+    })
+}
   
-  icons.forEach((list_item) => {
 
-    list_item.classList.add(list_item.innerText);
 
-    items.push({name: list_item.innerText})
-    const names = list_item.innerText+'.svg';
-    list_item.innerHTML = `
-      <span class="group">
-        <span class="icons"><img src="../../static/${$color}/${names}"></span>
-        <div class="name">${names}</div>
-        <span class="copy">Copy name below</span>
-        <br>
-        <input type="text" value="${$color}/${names.split('.svg')[0]}">
-      </span>
-    `
-  })
+
+// html output for form fields
+function getFieldHTML(name, type, options, attributes) {
+    let html=`<label for="${name}">${name} ${attributes.mandatory?'*':''}</label><br>`;
+    const r=attributes.mandatory?'required':'';
+    const ph=attributes.placeholder?` placeholder="${attributes.placeholder}"`:'';
+
+    if (type=='text') {
+        html+=`<input class="form-field" type="text" id="${name}" name="${name}" ${r} ${ph}><br>`;
+    }
+
+    if (type=='textarea') {
+        html+=`<textarea class="form-field" id="${name}" name="${name}" rows=${attributes.rows} ${r} ${ph}>`;
+    }
+
+    if (type=='radio') {
+        options.forEach((o) => {
+            html+=`<input class="form-field" type="radio" id="${name}" name="${name}" value="${o}" ${r}>
+            <label for="${name}">${o}</label><br>`
+        })
+    }
+    return (html);
 }
 
-const update_colors = ($color) => {
-  const icons = document.querySelectorAll('main li');
-  let extention = has_searched ?'.svg':''
-  icons.forEach((icon) => {
-    const name = icon.querySelector('.name').innerText;
-    icon.querySelector('img').setAttribute('src', `../../static/${$color}/${name}${extention}`)
-    icon.querySelector('input').value = `${$color}/${name}`
-  })
+// decorate a google sheets submitted form section
+
+function decorateForm () {
+    const sheetqs='main a[href^="https://docs.google.com/spreadsheets/"]';
+    document.querySelectorAll(sheetqs).forEach(($a) => {
+        const sheetid=$a.getAttribute('href').split('/')[5];
+        const $div=$a.parentNode.parentNode;
+        let thankyou='';
+        $a.setAttribute('href','javascript:');
+        $div.querySelectorAll('a').forEach(($diva) => {
+            if ($diva.innerHTML.toLowerCase().trim() == 'thank you') {
+                thankyou=$diva.getAttribute('href');
+                $diva.parentNode.remove();
+            }
+        })
+        $a.addEventListener('click', (e) => {
+            submitSheetForm($form, sheetid, thankyou)
+        });
+        $div.classList.add('form');
+        const $form=createTag('form');
+
+        $div.querySelectorAll(':scope > p').forEach(($f) => {
+            const $anchor=$f.querySelector('a');
+            const $placeholder=$f.querySelector('em');
+
+            if (!$anchor) {
+                const formfield=$f.firstChild.textContent;
+                let attributes={};
+                if (formfield.indexOf('*')) attributes.mandatory=true;
+                let type='text';
+                let options=[];
+                const name=formfield.split('*')[0].trim();
+    
+                if ($f.nextElementSibling) {
+                    $f.nextElementSibling.querySelectorAll('li').forEach(($li) => {
+                        options.push($li.innerHTML)
+                    });
+                    if (options.length>0) {
+                        $f.nextElementSibling.remove();
+                        type='radio';
+                    }
+                }
+    
+                if (formfield.indexOf('[')>0) {
+                    const descriptor=formfield.match(/\[(.*?)\]/)[1].toLowerCase().trim();
+                    if (descriptor.endsWith('lines')) {
+                        type='textarea'
+                        attributes.rows=descriptor.split(' ')[0];
+                    } else {
+                        type=descriptor;
+                    }
+                }
+
+                if ($placeholder) {
+                    attributes.placeholder=$placeholder.textContent;
+                }
+    
+                $f.innerHTML=getFieldHTML(name, type, options, attributes);    
+            }
+            $form.appendChild($f);
+        })
+        $div.appendChild($form);
+    })
 }
 
 
-const toggle_button = document.createElement('button');
-toggle_button.className = 'toggle_button';
-toggle_button.innerHTML = `<span></span>`
-document.body.append(toggle_button)
+function wrapSections(element) {
+    document.querySelectorAll(element).forEach(($div) => {
+        const $wrapper=createTag('div', { class: 'section-wrapper'});
+        $div.parentNode.appendChild($wrapper);
+        $wrapper.appendChild($div);
+    });
+}
 
 
-const script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://cdn.jsdelivr.net/npm/fuse.js@6.4.6';
-document.getElementsByTagName('head')[0].appendChild(script);
 
-const search_input = document.createElement('input');
-search_input.classList.add('search-field')
-search_input.setAttribute('placeholder', 'Search icons')
 
-document.querySelector('main ul').insertAdjacentHTML('beforebegin', search_input.outerHTML);
-
-const options = {
-  threshold: 0.5,
-  distance: 100,
-  keys: [
-    "name"
-  ]
+let debounce = function(func, wait, immediate) {
+	let timeout;
+	return function() {
+		let context = this, args = arguments;
+		let later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		let callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
 };
 
 
-const rewrite_icons = ($data) => {
-  document.querySelector('main ul').innerHTML = '';
-  const $color = document.body.classList.contains('dark') ? 'spectrum-icons-white': 'spectrum-icons-black';
-  let $list_item = ``;
-  
-  if($data.length > 0) {
-    $data.forEach(($item) => {
-      $list_item += `
-      <li>
-        <span class="group">
-          <span class="icons"><img src="../../static/${$color}/${$item.item.name}.svg"></span>
-          <div class="name">${$item.item.name}</div>
-          <span class="copy">Copy name below</span>
-          <br>
-          <input type="text" value="${$color}/${$item.item.name}">
-        </span>
-      </li>
-      `
-    })
-    document.querySelector('main ul').innerHTML = $list_item;
-  } else {
-    document.querySelector('main ul').innerHTML = `<h1>No matches found.</h1>`;
-  }
-  // console.log($list_item)
+// set fixed height to cards to create a uniform UI
+function cardHeightEqualizer($el) {
+    let initialHeight = 0;
+    let element = document.querySelectorAll($el);
+
+    if(window.innerWidth >= 700 && element.length > 1) {
+        element.forEach(function(card_el) {
+            card_el.style.height = 'auto';
+        })
+    
+        element.forEach(function(card_text) {
+            if(initialHeight < card_text.offsetHeight) {
+                initialHeight = card_text.offsetHeight;
+            }
+        })
+        
+        element.forEach(function(card_el) {
+            card_el.style.height = initialHeight + 'px';
+        })
+    } else {
+        element.forEach(function(card_el) {
+            card_el.style.height = 'auto';
+        })
+    }
 }
 
-window.addEventListener('load', () => {
-  const fuse = new Fuse(items, options)
-  document.querySelector('.search-field').addEventListener('keyup', function(event) {
-    has_searched = true;
-    // console.log(fuse.search(event.currentTarget.value), 'here')
-    if(event.currentTarget.value.length >= 1) {
-      rewrite_icons(fuse.search(event.currentTarget.value))
+
+function styleBackgrounds() {
+    let backgrounds = document.querySelectorAll('.background');
+
+    if(!backgrounds.length) return;
+    
+    backgrounds.forEach(function(background) {
+        if(!background.childNodes[0]) return;
+        if(background.childNodes[0].nodeName === "IMG") {
+            let src = background.childNodes[0].getAttribute('src')
+            background.style.backgroundImage = `url(${src})`;
+            background.innerHTML = ``;
+        }
+        
+    })
+}
+
+
+
+window.addEventListener('resize', debounce(function() {
+    // run resize events 
+    cardHeightEqualizer('.premiere .card .text');
+}, 250));
+
+
+
+
+  
+
+function addNavCarrot() {
+    if(document.querySelector('header img')) {
+        let svg = document.querySelector('header img');
+        let svgWithCarrot = document.createElement('div');
+        svgWithCarrot.classList.add('nav-logo');
+
+        svgWithCarrot.innerHTML = `
+        <span class="product-icon">
+            ${svg.outerHTML}
+        </span>
+
+        <span class="carrot">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+        </span>
+        `;
+        svg.remove();
+        document.querySelector('header div')
+        .prepend(svgWithCarrot);
+        document.querySelector('header').classList.add('default-nav')
+
+    
+
+        if(document.querySelector('header .section-wrapper p')) {
+            let productName = document.querySelector('header .section-wrapper').children[1].querySelector('p')
+            document.querySelector('.product-icon').appendChild(productName)            
+        }
+
     }
-  })
-})
+}
 
-toggle_button.addEventListener('click', () => {
-  document.body.classList.toggle('dark')
-  if(document.body.classList.contains('dark')) {
-    update_colors('spectrum-icons-white');
-  } else {
-    update_colors('spectrum-icons-black');
-  }
-})
 
-set_icon_layout('spectrum-icons-black');
+function dropDownMenu() {
+let $header = document.querySelector('header');
+
+if(window.outerWidth >= 768) return;
+
+if(!$header.classList.contains('nav-showing')) {
+    $header.querySelector('ul').style.display = 'flex';
+    $header.classList.add('nav-showing')
+} else {
+    $header.querySelector('ul').style.display = 'none';
+    $header.classList.remove('nav-showing')
+}
+}
+  
+  
+
+function paramHelper() {
+    if(!window.location.search) return;
+    let query_type = new URLSearchParams(window.location.search);
+
+    // Set Main Video
+    // make sure video indicator is being requested
+    if(query_type.get('v')) {
+        const v=query_type.get('v');
+        const $cards=document.querySelectorAll('.cards .card');
+        const $heroCard=$cards[v=='last'?$cards.length-1:+v-1];
+        $heroCard.className='main-video';
+        $heroCard.parentNode.prepend($heroCard);
+    }
+}
+
+async function decoratePage() {
+    addDefaultClass('main>div');
+    wrapSections('main>div');
+    await loadLocalHeader();
+    wrapSections('header>div');
+    wrapSections('footer>div');
+    window.pages.decorated = true;
+    paramHelper();
+    appearMain();
+}
+    
+if (document.readyState == 'loading') {
+    window.addEventListener('DOMContentLoaded', (event) => {
+        decoratePage();
+    });
+} else {
+    decoratePage();
+}
+
+loadJSModule('../../templates/cc-growth/icons.js');
