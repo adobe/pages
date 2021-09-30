5b554b1993c1f02f2e34efc176d8b5523dcde05a
Kenny Krosky <kenny.krosky@gmail.com>
2021-09-30 16:41:25 -0700

    Code clean up

--- pages/blocks/headerwithlockup/headerwithlockup.js
+++ pages/blocks/headerwithlockup/headerwithlockup.js
@@ -9,37 +9,46 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const header = document.querySelector('.headerwithlockup div');
-let hasText = null;
-let hasNav = null;
 
-if (header.querySelector('div:first-of-type').childElementCount > 1) {
-  header.querySelector('div:first-of-type').classList.add('has-text');
-  hasText = true;
-}
+const headerwithlockup = ($header) => {
+  const header = $header.querySelector('.headerwithlockup div');
 
-if (header.querySelector('div:last-of-type').childElementCount === 0) {
-  header.querySelector('div:last-of-type').remove();
-} else {
-  hasNav = true;
-  header.classList.add('has-nav');
-  if (hasText) header.classList.add('align-center');
-}
+  let hasIconWithText = null;
+  let hasListItems = null;
 
-if (hasNav) {
-  const mobileBtn = document.createElement('button');
-  mobileBtn.innerText = 'menu';
-  mobileBtn.classList.add('menu-button');
+  const $firstChild = header.querySelector('div:first-of-type');
+  const $lastChild = header.querySelector('div:last-of-type');
 
-  mobileBtn.innerHTML = `
-    <span class="sr-only">Menu</span>
-    <span></span>
-    <span></span>
-    <span></span>
-  `;
-  header.querySelector('div:first-of-type').appendChild(mobileBtn);
+  if ($firstChild && $firstChild.childElementCount > 1) {
+    $firstChild.classList.add('has-text');
+    hasIconWithText = true;
+  }
 
-  document.querySelector('.menu-button').addEventListener('click', () => {
-    document.querySelector('.headerwithlockup-container').classList.toggle('menu-showing');
-  });
-}
+  if (header.querySelector('div:last-of-type').childElementCount === 0) {
+    header.querySelector('div:last-of-type').remove();
+  } else if (!$lastChild.querySelector('ul')) {
+    header.querySelector('div:last-of-type').remove();
+    console.error('You need an UL (list item) in the right block');
+  } else {
+    hasListItems = true;
+    header.classList.add('has-nav');
+    if (hasIconWithText) header.classList.add('align-center');
+  }
+
+  if (hasListItems) {
+    const mobileBtn = document.createElement('button');
+    mobileBtn.innerText = 'menu';
+    mobileBtn.classList.add('menu-button');
+    mobileBtn.innerHTML = `
+      <span class="sr-only">Menu</span>
+      <span></span>
+      <span></span>
+      <span></span>
+    `;
+    $firstChild.appendChild(mobileBtn);
+    mobileBtn.addEventListener('click', () => {
+      document.querySelector('.headerwithlockup-container').classList.toggle('menu-showing');
+    });
+  }
+};
+export default headerwithlockup;
