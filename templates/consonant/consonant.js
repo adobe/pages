e288e9933fb52e11ea230286537aaa080981941f
Solene Duprat <solene.duprat@gmail.com>
2021-11-10 16:24:26 -0500

    fix(consonant): buttons styling are fixed

--- templates/consonant/consonant.js
+++ templates/consonant/consonant.js
@@ -676,7 +676,7 @@ export function buildArticleCard(article, type = 'article') {
 }
 
 export function decorateButtons(block = document) {
-  const $blocksWithoutButton = [];
+  const $blocksWithoutButton = ['header'];
   block.querySelectorAll(':scope a').forEach(($a) => {
     $a.title = $a.title || $a.textContent;
     const $block = $a.closest('div.section-wrapper > div > div');
