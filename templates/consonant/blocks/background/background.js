6ebef1f233b0f8bdd8bb21c4d8b1796d72a5e6fc
Solene Duprat <solene.duprat@gmail.com>
2021-11-11 13:58:43 -0500

    fix(consonant-blocks): variables for section spacing

--- templates/consonant/blocks/background/background.js
+++ templates/consonant/blocks/background/background.js
@@ -15,13 +15,12 @@ import {
 } from '../block-helpers.js';
 
 export default function decorate($block) {
+  const $blocksNoBackgroundPadding = ['marquee', 'separator'];
   const $background = $block.querySelector(':scope > div:first-of-type');
+  const $sectionWrapper = $block.closest('.section-wrapper');
   if ($background) {
     const color = $background.innerText;
-    const $sectionWrapper = $block.closest('.section-wrapper');
-    if ($sectionWrapper) {
-      $sectionWrapper.style.backgroundColor = color;
-    }
+    $sectionWrapper.style.backgroundColor = color;
   }
   const $otherCells = Array.from($block.querySelectorAll(':scope > div:not(:first-of-type)'));
   $otherCells.forEach(($cell) => {
@@ -30,4 +29,32 @@ export default function decorate($block) {
     }
   });
   $block.remove();
+  if ($background) {
+    const $blockFirstChild = $sectionWrapper.querySelector(':scope > div > :first-child');
+    const firstBlockNames = [];
+    if ($blockFirstChild) {
+      const firstBlockClassNamesString = $blockFirstChild.className;
+      const firstBlockClassNames = firstBlockClassNamesString.split(' ');
+      firstBlockClassNames.forEach((className) => {
+        firstBlockNames.push(className);
+      });
+    }
+    if (firstBlockNames.some((e) => $blocksNoBackgroundPadding.includes(e))) {
+      $blockFirstChild.style.marginTop = '0';
+      $sectionWrapper.style.paddingTop = '0';
+    }
+    const $blockLastChild = $sectionWrapper.querySelector(':scope > div > :last-child');
+    const lastBlockNames = [];
+    if ($blockLastChild) {
+      const lastBlockClassNamesString = $blockLastChild.className;
+      const lastBlockClassNames = lastBlockClassNamesString.split(' ');
+      lastBlockClassNames.forEach((className) => {
+        lastBlockNames.push(className);
+      });
+    }
+    if (lastBlockNames.some((e) => $blocksNoBackgroundPadding.includes(e))) {
+      $blockLastChild.style.marginBottom = '0';
+      $sectionWrapper.style.paddingBottom = '0';
+    }
+  }
 }
