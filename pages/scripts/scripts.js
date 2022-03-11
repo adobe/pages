83f92a8644e6d94b0fa98e64630c898ce5215dc2
Max Edell <maxed@adobe.com>
2022-03-11 14:11:21 -0800

    use innerText for template name

--- pages/scripts/scripts.js
+++ pages/scripts/scripts.js
@@ -886,7 +886,7 @@ export function getTemplateName() {
     return undefined;
   }
 
-  const template = toClassName($template.textContent);
+  const template = toClassName($template.innerText);
   $template.remove();
   return template;
 }
