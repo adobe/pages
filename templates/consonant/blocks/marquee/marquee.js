0c342eb2d7218bd03816bd8f0c6d325ae6084dcf
Solène Duprat <solene.duprat@gmail.com>
2022-04-13 18:10:09 -0400

    wip: consonant 5.0 blocks

--- templates/consonant/blocks/marquee/marquee.js
+++ templates/consonant/blocks/marquee/marquee.js
@@ -14,10 +14,19 @@ import {
 } from '../../consonant.js';
 
 export default function decorate($block) {
+  const $pics = $block.querySelectorAll('picture');
+  $pics.forEach((pic) => {
+    if (pic.parentElement.tagName === 'P') {
+      // unwrap single picture if wrapped in p tag
+      const $parentDiv = pic.closest('div');
+      const $parentParagraph = pic.parentNode;
+      $parentDiv.insertBefore(pic, $parentParagraph);
+    }
+  });
   const $sectionWrapper = $block.closest('.marquee-container');
-  const bg = $block.querySelector(':scope > div:first-of-type > div');
+  const bg = $block.querySelector(':scope > div:first-of-type');
   bg.classList.add('background');
-  let bgImg = bg.querySelector(':scope img');
+  let bgImg = bg.querySelector('picture');
   // Set background to text value if there is no image:
   if (!bgImg) {
     const $a = bg.querySelector('a');
@@ -28,15 +37,23 @@ export default function decorate($block) {
       bg.style.background = bg.textContent;
       bg.innerHTML = '';
     }
+  } else if (bgImg.parentElement.tagName.toLowerCase === 'p') {
+    bgImg.parentElement.parentElement.appendChild(bgImg);
   }
   const content = $block.querySelector(':scope > div:nth-of-type(2)');
   content.classList.add('container');
-  content.querySelector(':scope > div:first-of-type').classList.add('marquee-column');
-  content.querySelector(':scope > div:last-of-type').classList.add('marquee-column');
-  const picElement = content.querySelector(':scope picture');
-  if (picElement) {
-    picElement.parentElement.classList.add('marquee-image');
-  }
+  const $cells = Array.from(content.children);
+  $cells.forEach(($cell) => {
+    $cell.classList.add('marquee-column');
+    const picElement = $cell.querySelector('picture');
+    if (picElement) {
+      $cell.classList.add('marquee-image');
+    }
+
+    // remove empty p from empty columns
+    const $emptyP = $cell.querySelector(':scope > p:first-child:last-child');
+    if ($emptyP && $emptyP.childNodes.length === 0) $emptyP.remove();
+  });
   const caption = $block.querySelector(':scope > div:nth-of-type(3)');
   if (caption) {
     caption.classList.add('marquee-caption');
@@ -46,8 +63,8 @@ export default function decorate($block) {
   if ($previousSection) {
     if ($previousSection.classList.contains('section-metadata-container')) {
       Array.from($previousSection.children).forEach(($row) => {
-        const $cells = Array.from($row.children);
-        if ($cells[0] && $cells[1] && (/background/gi).test($cells[0].innerText)) {
+        const $prev = Array.from($row.children);
+        if ($prev[0] && $prev[1] && (/background/gi).test($prev[0].innerText)) {
           $previousSection.classList.add('padding-bottom-L');
         }
       });
