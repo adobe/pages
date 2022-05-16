4a1591846f4623277e7a1d83f37f4681dde50f09
Solène Duprat <solene.duprat@gmail.com>
2022-05-16 19:45:31 -0400

    pages-artisthub bug fixes

--- templates/consonant/consonant.js
+++ templates/consonant/consonant.js
@@ -492,22 +492,22 @@ export function decorateButtons(block = document) {
       const $p = $a.closest('p');
       if ($p) {
         const childNodes = Array.from($p.childNodes);
-        const re = new RegExp('^\\s*$');
-
+        const whitespace = new RegExp('^\\s*$');
+        // Check that the 'button-container' contains buttons only
         const buttonsOnly = childNodes.every(($c) => {
-          if (isNodeName($c, 'a') || (isNodeName($c, '#text') && re.test($c.textContent))) {
+          if (isNodeName($c, 'a') || (isNodeName($c, '#text') && whitespace.test($c.textContent))) {
             return true;
           } else if ($c.childNodes.length > 0) {
             return Array.from($c.childNodes).every(($cc) => {
-              if (isNodeName($cc, 'a') || (isNodeName($cc, '#text') && re.test($cc.textContent))) {
+              if (isNodeName($cc, 'a') || (isNodeName($cc, '#text') && whitespace.test($cc.textContent))) {
                 return true;
               } else if ($cc.childNodes.length > 0) {
-                return Array.from($cc.childNodes).every(($ccc) => isNodeName($ccc, 'a') || (isNodeName($ccc, '#text') && re.test($ccc.textContent)));
+                // Could be nested twice for 'em' and 'strong' tags.
+                return Array.from($cc.childNodes).every(($ccc) => isNodeName($ccc, 'a') || (isNodeName($ccc, '#text') && whitespace.test($ccc.textContent)));
               } else return false;
             });
           } else return false;
         });
-
         if (buttonsOnly) {
           $p.classList.add('button-container');
           const $up = $a.parentElement;
