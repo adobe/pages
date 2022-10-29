96f8502f6e19c135b72db78f2709cae11f77afc2
Jasper Stephenson <jasperstephenson@gmail.com>
2022-10-29 09:45:26 +0900

    Add WIP version of which-app template for initial QA

---
+++ templates/which-app/decorators/decorateCategory.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+export default function decorateCategory() {
+  const centeredIntro = document.querySelector(
+    '.centeredintro',
+  );
+  if (!centeredIntro) return;
+  centeredIntro.classList.add('columns');
+
+  const doubleVideo = document.querySelector('.doublevideo');
+  doubleVideo.classList.add('columns');
+
+  const taskSelector = document.querySelector('.taskselector');
+  taskSelector.classList.add('columns');
+
+  // wrap all but the first child in a div
+  const taskSelectorChildrenWrapper = document.createElement('div');
+  taskSelectorChildrenWrapper.classList.add('tasks');
+  [...(taskSelector?.children || [])]
+    .slice(1)
+    .forEach((child) => {
+      taskSelectorChildrenWrapper.appendChild(child);
+    });
+  taskSelector?.appendChild(taskSelectorChildrenWrapper);
+  const allButton = document.createElement('div');
+  allButton.innerHTML = 'All';
+  allButton.classList.add('taskall');
+  taskSelectorChildrenWrapper.prepend(allButton);
+
+  const categorySections = document.querySelectorAll(
+    '.categorysection',
+  );
+  categorySections.forEach((categorySection) => {
+    categorySection.classList.add('columns');
+  });
+}
---
+++ templates/which-app/decorators/decorateLanding.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+export default function decorateLanding() {
+  const introSection = document.querySelector(
+    '.landingintrosection',
+  );
+  if (!introSection) return;
+  introSection.classList.add('columns');
+
+  const categoryHeader = document.querySelector(
+    '.landingcategoryheader',
+  );
+  categoryHeader.classList.add('columns');
+  // flattenDom(introSection, 1)
+
+  const categories = document.querySelectorAll(
+    '.landingcategories > div',
+  );
+  const initialPathMinusHashAndTrailingSlash = window.location.pathname
+    .replace(/\/$/, '')
+    .replace(/#.*$/, '');
+  categories.forEach((category) => {
+    const headerContent = category.querySelector('h1')?.innerHTML;
+    category.addEventListener('click', () => {
+      window.location.href = `${initialPathMinusHashAndTrailingSlash}/${headerContent
+        .toLowerCase()
+        .replace(/ /g, '')}`;
+    });
+  });
+}
